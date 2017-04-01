d138d62a6c587655d43edaa5dd5b675bf22abe7f
apiromz <apirom.z@hotmail.com>
2017-04-01 14:54:29 +0700

    no message

--- server/models/eval.js
+++ server/models/eval.js
@@ -6,5 +6,10 @@ module.exports = mongoose.model('Eval',{
 	reAHP:Array,
 	weight:Array,
 	cf_regression:Array,
-	assrule_cf:Array
+	assrule_cf:Array,
+	ndcg_concat:{ type: Number, default: 0 },
+	ndcg_reAHP:{ type: Number, default: 0 },
+	ndcg_weight:{ type: Number, default: 0 },
+	ndcg_cf_regression:{ type: Number, default: 0 },
+	ndcg_assrule_cf:{ type: Number, default: 0 },
 });
