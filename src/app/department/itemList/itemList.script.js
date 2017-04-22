88ba1c0cc1193524e218ff4f18f0309e03cf482b
apiromz <apirom.z@hotmail.com>
2017-04-22 21:26:28 +0700

    no message

--- src/app/department/itemList/itemList.script.js
+++ src/app/department/itemList/itemList.script.js
@@ -5,26 +5,6 @@ $(document).ready(function () {
     console.log('filter click');
   })
 
-  $('.owl-carousel').owlCarousel({
-      loop:true,
-        margin:10,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            600:{
-                items:3,
-                nav:false
-            },
-            1000:{
-                items:5,
-                nav:true,
-                loop:false
-            }
-        }
-  });
 	// $('#scroll-top').click(function(){
 	// 	console.log("hi pagination");
 	// 	$("html, body").animate({scrollTop: 0}, 750);
